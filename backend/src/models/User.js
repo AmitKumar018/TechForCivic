91d2fe033259d49a6629f92f48bdcacb9e760a05
AmitKumar018 <your-email@example.com>
2025-11-24 07:55:39 +0530

    appplied email validation

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -5,7 +5,17 @@ const MAX_ADMINS = 2; // hardcoded safety net
 const userSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true, lowercase: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Please enter a valid email address",
+      ],
+    },
+
     password: { type: String, required: true },
     role: { type: String, enum: ["citizen", "admin"], default: "citizen" },
   },
@@ -15,7 +25,9 @@ const userSchema = new mongoose.Schema(
 //  Pre-save hook to prevent creating more than MAX_ADMINS
 userSchema.pre("save", async function (next) {
   if (this.role === "admin" && this.isNew) {
-    const adminCount = await mongoose.models.User.countDocuments({ role: "admin" });
+    const adminCount = await mongoose.models.User.countDocuments({
+      role: "admin",
+    });
     if (adminCount >= MAX_ADMINS) {
       return next(new Error("Admin limit reached — cannot add more admins"));
     }
